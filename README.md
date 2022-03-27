9922b21e1a0014fe81dfe7c20ad1fe8ea08e6112
Manh Nguyen <manhnb1911@gmail.com>
2022-03-27 21:57:53 +0700

    update requirements.txt, README.md

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 1. `$pip install -r requirements.txt`
 2. `$export TOKEN=<your-bot-token>`
-3. `$python main.py`
+3. `$export BOT_ID=<your-bot-id>`
+4. `$export OWNER_ID=<your-discord-id>`
+5. `$python main.py`
--- main.py
+++ main.py
@@ -4,8 +4,5 @@ from multiprocessing import Process
 from bot import bot
 from web import app
 
-from logging import getLogger
-getLogger("werkzeug").disabled = True
-
-Process(target=lambda: app.run("0.0.0.0", 8080)).start()
+Process(target=lambda: app.run("0.0.0.0")).start()
 bot.run(os.environ["TOKEN"])
