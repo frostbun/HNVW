dfa803e1cedad3dc25efaf3056371fe61fbc9bf1
Manh Nguyen <manhnb1911@gmail.com>
2025-07-28 16:24:32 +0700

    update van mau

--- ditmewibu/util/van_mau.py
+++ ditmewibu/util/van_mau.py
@@ -2,21 +2,30 @@ import random
 
 import requests
 
-van_mau = [line.strip() for line in requests.get("https://raw.githubusercontent.com/comgachienmam147/comgachienmam147.github.io/refs/heads/master/copypasta.txt").text.split("********************")]
+van_mau_list = [
+    line.strip()
+    for line in requests.get(
+        "https://raw.githubusercontent.com/comgachienmam147/comgachienmam147.github.io/refs/heads/master/copypasta.txt"
+    ).text.split("********************")
+]
 
-for i in range(len(van_mau)):
-    van_mau[i] = "\n".join(van_mau[i].split("\n")[4:])
+van_mau_list = [
+    " ".join(line.strip() for line in van_mau.split("\r\n")[4:])
+    for van_mau in van_mau_list
+]
 
-van_mau = van_mau[:-1]
+van_mau_list = [van_mau for van_mau in van_mau_list if van_mau != ""]
+
 
 def get_van_mau():
-    return random.choice(van_mau)
+    return random.choice(van_mau_list)
+
 
 if __name__ == "__main__":
-    print(len(van_mau))
+    print(len(van_mau_list))
     print("********************")
-    print(van_mau[0])
+    print(van_mau_list[0])
     print("********************")
-    print(van_mau[-1])
+    print(van_mau_list[-1])
     print("********************")
     print(get_van_mau())
