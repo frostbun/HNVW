b26252620837dfcd6540dabef5711db13c1b7003
Manh Nguyen <manhnb1911@gmail.com>
2025-07-28 15:48:53 +0700

    migrate HNVW to slash command

--- hnvw/cog/game/__init__.py
+++ hnvw/cog/game/__init__.py
@@ -1,5 +1,6 @@
 from discord import Member
-from discord.ext.commands import Cog, command
+from discord.commands import Option, slash_command
+from discord.ext.commands import Cog
 
 from .tictactoe import TicTacToe
 
@@ -9,6 +10,6 @@ class Game(Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @command(brief="Challenge a member to a tic-tac-toe game")
-    async def tictactoe(self, ctx, member: Member):
+    @slash_command(description="Challenge a member to a tic-tac-toe game")
+    async def tictactoe(self, ctx, member: Option(Member, "Recipient")):
         await TicTacToe(ctx, member).start()
